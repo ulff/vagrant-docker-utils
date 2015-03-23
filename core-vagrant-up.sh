ab07de7b3441487a7f0c08e9d5b2a0ba14aef5f1
jacekelgda <jacekelgda@gmail.com>
2015-03-23 15:32:52 +0100

    interactiv setup

--- core-vagrant-up.sh
+++ core-vagrant-up.sh
@@ -1,11 +1,52 @@
 #!/bin/bash
+# Set PS3 prompt
 
 modman clone git@github.com:Sysla/ubuntu1410-docker.git
 modman update-all
 modman deploy-all
 
 echo "creating proxy/config.yaml"
-cp proxy/config.yaml.TEMPLATE proxy/config.yaml
+
+IP=192.168.36.10
+PORT=8080
+VM_HOSTNAME="devstack.dev"
+
+PS3="Enter the space shuttle to get more information : "
+
+select hostname in devstack maritime sysla offshore
+do
+    case $hostname in
+    devstack)
+      IP=192.168.36.10
+      PORT=8080
+      VM_HOSTNAME=devstack.dev
+      break
+      ;;
+    maritime)
+      IP=192.168.36.11
+      PORT=8081
+      VM_HOSTNAME=maritime.no
+      break
+      ;;
+    sysla)
+      IP=192.168.36.12
+      PORT=8082
+      VM_HOSTNAME=sysla.no
+      break
+      ;;
+    offshore)
+      IP=192.168.36.13
+      PORT=8083
+      VM_HOSTNAME=offshore.no
+      break
+      ;;
+    *)
+      echo "Error: Please try again"
+      ;;
+  esac
+done
+
+sed "s/<HOSTNAME>/$VM_HOSTNAME/;s/<IP_ADDRESS>/$IP/;s/<PORT>/$PORT/" < proxy/config.yaml.TEMPLATE > proxy/config.yaml
 vi proxy/config.yaml
 
 # Wordpress specific setup

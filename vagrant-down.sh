bd9dc9e0d495619860a6e61c69e8da8ad004de0f
jacekelgda <jacekelgda@gmail.com>
2015-04-10 11:50:18 +0200

    Remove vendors on machine down

--- vagrant-down.sh
+++ vagrant-down.sh
@@ -14,3 +14,12 @@ fi
 if [ -f "local-config.php" ]; then
   rm local-config.php
 fi
+
+if [ -f "composer.json" ]; then
+  rm -fr vendor
+  rm -fr wp
+fi
+
+if [ -f "package.json" ]; then
+  rm -fr node_modules
+fi

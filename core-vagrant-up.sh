09f67c26281560e17cccd5eafa94cd75f749f184
jacekelgda <jacekelgda@gmail.com>
2015-04-10 11:53:28 +0200

    Remove vendors on machine up

--- core-vagrant-up.sh
+++ core-vagrant-up.sh
@@ -102,6 +102,15 @@ if [ -f ".dockercfg.TEMPLATE" ]; then
   fi
 fi
 
+if [ -f "composer.json" ]; then
+  rm -fr vendor
+  rm -fr wp
+fi
+
+if [ -f "package.json" ]; then
+  rm -fr node_modules
+fi
+
 vagrant up --provider=docker --no-parallel
 
 location=$(pwd)

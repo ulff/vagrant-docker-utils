202e573f9aa79aa476fcea10bba76d7f7fde838e
jacekelgda <jacekelgda@gmail.com>
2015-04-10 16:04:34 +0200

    proxy can be destroyed n change

--- vagrant-reload.sh
+++ vagrant-reload.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-vagrant destroy -f
-
-modman remove ubuntu1410-docker
-modman remove vagrant-docker-utils
-
-if [ -f "proxy/config.yaml" ]; then
-  rm proxy/config.yaml
-fi
-
-if [ -f "local-config.php" ]; then
-  rm local-config.php
-fi
-
+sh bin/vagrant-down.sh
 sh bin/vagrant-up.sh auto
